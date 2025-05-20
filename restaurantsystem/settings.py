5f10ed0ce437a2a4dc1d4e89e66047b58770116d
Camilo Acevedo <108965135+camidev234@users.noreply.github.com>
2025-05-19 21:31:00 -0500

    set settings

--- restaurantsystem/settings.py
+++ restaurantsystem/settings.py
@@ -137,8 +137,10 @@ DATABASES = {
         'USER': os.getenv("POSTGRES_USER"),
         'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
-        'PORT': os.getenv("POSTGRES_PORT")
-    }
+        'PORT': os.getenv("POSTGRES_PORT", "5432"),
+        
+    },
+    
 }
 
 
